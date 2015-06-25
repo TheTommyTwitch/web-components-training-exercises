027c0dd35f414dd202e18443b3b74bd2b3b92798
Thomas Asadurian <Tasadurian@gmail.com>
2015-06-25 13:11:15 -0700

    slider ex

--- styleguide/customElement.js
+++ styleguide/customElement.js
@@ -1,10 +1,10 @@
-var createElement = function(tagName, basePrototype){
+var createElement = function(template, basePrototype){
 	basePrototype = basePrototype || HTMLElement.basePrototype;
 	var template = document.currentScript.ownerDocument.getElementById(template).content;
 	var customElementPrototype = Object.create(basePrototype);
 	customElementPrototype.createCallback = function() {
 		var shadowRoot = this.createShadowrRoot();
-		car clone = document.importNode(template, true);
+		var clone = document.importNode(template, true);
 		shadowRoot.appendChild(clone);
 	};
 	return document.registerElement(tagName, {
